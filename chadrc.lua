b8de25c66fb2c937fab9fd0a06c98a6e85b15ae0
whj768702 <whj768702@gmail.com>
2023-12-06 09:43:57 +0800

    init

---
+++ chadrc.lua
@@ -0,0 +1,26 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "kanagawa",
+  theme_toggle = { "kanagawa", "one_light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+  
+  transparency = true,
+
+  nvdash = {
+    load_on_startup = true,
+  }
+}
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+return M
